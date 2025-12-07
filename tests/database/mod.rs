df17bc6c2468ad6c8d564c977f6110bd36725e3b
RedRing <takatsu.noriyuki@gmail.com>
2025-12-07 13:55:18 +0900

    fix(tests): Resolve all test compilation warnings
    - Add #[allow(dead_code)] to mysql_security_test_runner.rs
    
    - Fix module_inception warnings by renaming test modules to 'tests'
    
    - All 693 tests passing with zero warnings

--- tests/database/mod.rs
+++ tests/database/mod.rs
@@ -12,14 +12,14 @@ mod mysql_phase1_integration_tests;
 mod mysql_phase1_tests;
 mod mysql_postgres_compatibility_tests;
 mod mysql_security_basic_tests;
-mod mysql_security_tests;
 mod mysql_security_test_runner;
+mod mysql_security_tests;
 
 // PostgreSQL テスト
-mod postgresql_integration_tests;
 mod postgres_database_integration_tests;
 mod postgres_phase2_basic_tests;
 mod postgres_phase2_integration_tests;
+mod postgresql_integration_tests;
 
 // その他のデータベース
 mod mariadb_integration_tests;
